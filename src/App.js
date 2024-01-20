055999af0ecbae2c4fd5ec0170379012dcbb60b9
Kamalesh <kamalesh5112000@pec.edu.com>
2024-01-20 15:07:09 +0530

    ExpenseForm commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Expenses from './components/Expenses/Expense';
+import NewExpense from './components/NewExpense/NewExpense';
 const App=()=> {
   const expenses = [
     {
@@ -27,7 +28,7 @@ const App=()=> {
   ];
   return (
     <div className='App'>
-    <h2>Let's get started!</h2>
+    <NewExpense />
     <Expenses items={expenses} />
   </div>
   );
